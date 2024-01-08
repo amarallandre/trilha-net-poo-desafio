e7e7ac9fab1a4cc9de9e2790ae90262c1a176caf
amarallandre <111255294+amarallandre@users.noreply.github.com>
2024-01-08 17:35:00 -0300

    desafio completo

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using DesafioPOO.Models;
+
+Console.WriteLine("Nokia");
+Smartphone nokia = new Nokia(numero: "123", modelo: "modelo", imei: "123", memoria: 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("facebook");
+
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone(numero: "123", modelo: "Modelo", imei: "222222222", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
